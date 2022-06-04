76768eece0470210051bafb853693158352502b2
snguyenhung <hung.nguyen@kellogg.northwestern.edu>
2022-06-04 00:19:59 +0000

    done mba

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,9 @@ class PostsController < ApplicationController
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post["place_id"] = params["post"]["place_id"]
+    @post["user_id"] = @current_user["id"]
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by({ "id" => params["id"] })
+  end
+  
   def new
     @user = User.new
   end
 
   def create
     @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+    @user["username"] = params["user"]["username"]
     @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
-    redirect_to "/"
+    redirect_to "/places"
   end
 end
